8e7e4333f26fb8a327c6963f59ecf4f312a2c303
zhubeilife <zhubeilife@gmail.com>
2022-02-01 10:16:23 +0800

    add koa-router

--- Liaoxuefeng/koa/app.js
+++ Liaoxuefeng/koa/app.js
@@ -4,6 +4,8 @@ const Koa = require('koa')
 
 const app = new Koa();
 
+const router = require('koa-router')();
+
 app.use(async (ctx, next) => {
     console.log(`Log1 ${ctx.request.method} ${ctx.request.url}`); // 打印URL
     await next(); // 调用下一个middleware
@@ -16,11 +18,17 @@ app.use(async (ctx, next) => {
     console.log(`Log2 Time: ${ms}ms`); // 打印耗费时间
 });
 
-app.use(async (ctx, next) => {
-    await next(); // if next not exist just running above
-    console.log('Log3')
-    ctx.response.type = 'text/html';
-    ctx.response.body = '<h1>Hello, koa2!</h1>';
-});
+router.get('/hello/:name', async (ctx, next) => {
+    var name = ctx.params.name;
+    ctx.response.body = `<h1>Hello, ${name}</h1>`;
+    console.log('Log3');
+})
+
+router.get('/', async (ctx, next) => {
+    ctx.response.body = '<h1>Index</h1>';
+    console.log('Log3');
+})
+
+app.use(router.routes());
 
 app.listen(8080);
